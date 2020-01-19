6a9059712bb964fea41a8e49114a8a4f15b7f78c
Ryan Wilson <rjwilson@terpmail.umd.edu>
2020-01-19 00:22:19 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,12 +1,13 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
+require 'pry'
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
 
 end
-
+binding.pry
 # Write a method that, given an NDS creates a new Hash
 # The return value should be like:
 #
@@ -15,3 +16,4 @@ def directors_totals(nds)
   result = {}
   nil
 end
+bindi
